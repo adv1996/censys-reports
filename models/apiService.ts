eb1bb713c879fdbb7ea1698606ce26e2a39daf80
Advaith Venkatakrishnan <advaith96@gmail.com>
2022-04-07 09:19:23 -0400

    added prod route

--- models/apiService.ts
+++ models/apiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api/'
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'https://censys-reports-cwsekk2m9-adv1996.vercel.app/api/'
 
 export interface ApiResponse<T> {
   errorMessage?: string
